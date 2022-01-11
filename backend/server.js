36bb5c7fd6746db9afb08bcb9f9f51537f1dcfc9
Genne23v <59734889+Genne23v@users.noreply.github.com>
2022-01-10 23:49:39 -0500

    broken image test

--- backend/server.js
+++ backend/server.js
@@ -24,13 +24,13 @@ app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
 app.use('/api/upload', uploadRoutes);
-
 app.get('/api/config/paypal', (req, res) =>
     res.send(process.env.PAYPAL_CLIENT_ID),
 );
 
 const __dirname = path.resolve();
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+console.log('dirname', __dirname);
+app.use('/uploads', express.static(path.join(__dirname + '/uploads')));
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '/frontend/build')));
